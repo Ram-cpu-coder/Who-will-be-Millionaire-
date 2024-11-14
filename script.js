1ef7bd8a29deb669e844ec1cd4f9ef29202fc9d9
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-15 10:50:40 +1100

    back button functionality

--- script.js
+++ script.js
@@ -2,14 +2,31 @@ const btn = document.querySelector("#btn");
 // console.log(btn);
 const container_1 = document.querySelector(".container-fluid-1");
 // console.log(container_1);
-btn.addEventListener("click", () => {
+// const back_button = document.querySelector("#back");
+
+const start = () => {
   console.log("clicked");
 
-  container_1.innerHTML = ` <div
+  container_1.innerHTML = `  <div class="fixed-top mt-5">
+      <button type="button" class="btn btn-primary btn-sm" onclick = "back()">
+        <i class="fa-regular fa-circle-left" id="back"></i>
+      </button>
+    </div>
+    <div
         class="container d-flex flex-column justify-content-center align-items-center"
       >
         <img src="./log.png" alt="img" />
         <h1 class="m-5 text-center">Who wants to be Millionaire?</h1>
         <button type="button" class="btn btn-primary p-3">START</button>
       </div>`;
-});
+};
+
+const back = () => {
+  console.log("clicked");
+  container_1.innerHTML = `<div class="container-fluid container-fluid-1 d-flex flex-column justify-content-center align-items-center">
+      <button type="button" class="btn btn-primary btn-lg" id="btn" onclick = "start()">
+        ENTER
+      </button>
+
+    </div>`
+}
