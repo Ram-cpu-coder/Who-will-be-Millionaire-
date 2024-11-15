7dea71ad37a8b8f782a1dc3d44f7b7db7444f975
Ram Kumar Dhimal <ramkumardhimal2058@gmail.com>
2024-11-16 10:51:26 +1100

    some changes

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 const btn = document.querySelector("#btn");
-// console.log(btn);
 const container_1 = document.querySelector(".container-fluid-1");
 const main_page = document.querySelector(".main-page");
-console.log(main_page);
 
-// console.log(container_1);
-// const back_button = document.querySelector("#back");
+
 
 const start = () => {
   console.log("clicked");
 
-  main_page.innerHTML = `  <div class="fixed-top back">
+  main_page.innerHTML = `  
+  <div class="fixed-top back">
       <button type="button" class="btn btn-primary btn-sm button-back" onclick = "back()">
         <i class="fa-regular fa-circle-left" id="back"></i>
       </button>
     </div>
     <div class="container-fluid container-fluid-1 d-flex flex-column justify-content-center align-items-center">
+    <div class="d-flex flex-column justify-content-center align-items-center" id = "main-page-bg" style = "height: 100vh">
         <img src="./log.png" alt="img" />
         <h1 class="m-5 text-center">Who wants to be Millionaire?</h1>
         <button type="button" class="btn btn-primary p-3">START</button>
+      </div>
       </div>`;
 
 };
